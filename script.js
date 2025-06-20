97427c7a1f97776e9bcf585766c26aeaf1e1350e
TitanOfTech24 <bazan.andres1997@gmail.com>
2025-06-19 22:11:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const turnos = [
   { fecha: "2025-06-21", hora: "12:00", ocupado: false }
 ];
 
-const formsBaseURL = const formsBaseURL = "https://forms.office.com/Pages/ResponsePage.aspx?id=yDY_H864YEK7LIsSHgEyqhFzcy0O6J5Dsxa0oIPDhi1UMzAwUUM5UTUwM1ZRQUtKSVJCNzUyMTI1SS4u"; // Reemplazar con tu URL real
+const formsBaseURL = "https://forms.office.com/Pages/ResponsePage.aspx?id=yDY_H864YEK7LIsSHgEyqhFzcy0O6J5Dsxa0oIPDhi1UMzAwUUM5UTUwM1ZRQUtKSVJCNzUyMTI1SS4u"; // Reemplazar con tu URL real
 
 const tabla = document.getElementById("tabla-turnos");
 
